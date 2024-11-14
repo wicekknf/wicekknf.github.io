3ecc09a5182bfd76ee425e977c3905b05bec41aa
Wiktor Łaziński <lazinski.w@wp.pl>
2024-11-14 17:26:42 +0100

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+const navBottom = document.querySelector('.nav-bottom');
+const burgerBtn = document.querySelector('.burger-btn');
+const bars = document.querySelector('.bars')
+const barsX = document.querySelector('.bars-x')
+const navBottomLinks = document.querySelectorAll('.nav-bottom a')
+const year = document.querySelector('.year')
+
+const currentYear = new Date().getFullYear();
+
+year.textContent = currentYear;
+
+const showMiniNav = () => {
+    bars.classList.toggle('hide-bars')
+    if (bars.classList.contains('hide-bars')) {
+        barsX.classList.add('active-bars-x')
+        navBottom.classList.add('show-miniNav')
+    } else {
+        barsX.classList.remove('active-bars-x')
+        navBottom.classList.remove('show-miniNav')
+    }
+}
+
+burgerBtn.addEventListener('click', showMiniNav);
