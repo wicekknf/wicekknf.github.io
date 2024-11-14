b95d6a18bc0507224dc2b6f476c9d8b39565b437
Wiktor Łaziński <lazinski.w@wp.pl>
2024-11-14 20:15:43 +0100

    working on index

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,25 @@
-const navBottom = document.querySelector('.nav-bottom');
-const burgerBtn = document.querySelector('.burger-btn');
-const bars = document.querySelector('.bars')
-const barsX = document.querySelector('.bars-x')
-const navBottomLinks = document.querySelectorAll('.nav-bottom a')
-const year = document.querySelector('.year')
+const navBottom = document.querySelector(".nav-bottom");
+const burgerBtn = document.querySelector(".burger-btn");
+const bars = document.querySelector(".bars");
+const barsX = document.querySelector(".bars-x");
+const navBottomLinks = document.querySelectorAll(".menu-list a");
+const year = document.querySelector(".year");
+
+console.log(navBottomLinks);
 
 const currentYear = new Date().getFullYear();
 
 year.textContent = currentYear;
 
 const showMiniNav = () => {
-    bars.classList.toggle('hide-bars')
-    if (bars.classList.contains('hide-bars')) {
-        barsX.classList.add('active-bars-x')
-        navBottom.classList.add('show-miniNav')
-    } else {
-        barsX.classList.remove('active-bars-x')
-        navBottom.classList.remove('show-miniNav')
-    }
-}
+	bars.classList.toggle("hide-bars");
+	if (bars.classList.contains("hide-bars")) {
+		barsX.classList.add("active-bars-x");
+		navBottom.classList.add("show-miniNav");
+	} else {
+		barsX.classList.remove("active-bars-x");
+		navBottom.classList.remove("show-miniNav");
+	}
+};
 
-burgerBtn.addEventListener('click', showMiniNav);
+burgerBtn.addEventListener("click", showMiniNav);
